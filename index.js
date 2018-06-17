a15f0e78fc6f2cdc8cb1b964395af9683dd484c7
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-17 18:32:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', '{{description}}', '{{#each ingredients}}');
+  Handlebars.registerHelper('displayIngredient');
+  Handlebars.registerPartial('recipeFormPartial', '{{name}}', '{{description}}', '{{ingredients}}');
+
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
